42b45b26903b80f6ecdb17769e66d3b163e07135
Abhigyan Prakash <44674401+mountAP@users.noreply.github.com>
2020-08-06 19:40:19 +0530

    Update JSON.js
    serialization & de-serialization

--- JSON.js
+++ JSON.js
@@ -9,15 +9,18 @@ let worldsBestDetective = {
     worstEnemy: 'James Moriarty',
 };
 
-// converts an object to JSON string
+// serialization
+// converting a native object to string
 let SherlocksInfo = JSON.stringify(worldsBestDetective);
 console.log(typeof SherlocksInfo === 'string'); // -> true
 
-// one possible application is to store this 
-// string in the local storage against a key
+// Applications:
+// 1. Useful when we want to tranmit data over network; or
+// 2. We can store this string in the local storage against a key
 // localStorage.setItem('detective', SherlocksInfo);
 
 
-// converts a JSON string to an object
+// deserialization
+// converting a string to a native object
 let myFavDetective = JSON.parse(SherlocksInfo);
 console.log(typeof myFavDetective === 'object'); // -> true
